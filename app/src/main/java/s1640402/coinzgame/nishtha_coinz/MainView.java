1637f1620c010447ccc59ee1838310db1ae355e1
s1640402 <s1640402@sms.ed.ac.uk>
2018-10-12 13:57:41 +0100

    Fixed center of map, added main menu and location services.

--- app/src/main/java/s1640402/coinzgame/nishtha_coinz/MainView.java
+++ app/src/main/java/s1640402/coinzgame/nishtha_coinz/MainView.java
@@ -2,63 +2,25 @@ package s1640402.coinzgame.nishtha_coinz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.Mapbox;
-
-
+import android.view.*;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MainView extends AppCompatActivity {
-
-    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance(this, "pk.eyJ1IjoibmlzaHRoYWt1bWFyIiwiYSI6ImNqbW5rbXdlaDBzYmYza254eGE1aXJkN2wifQ.Y2hUSRk2rGB45RKqgycCXQ");
         setContentView(R.layout.activity_main_view);
-        mapView = (MapView) findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        mapView.onStart();
+    public void playgame(View view){
+        Intent intent = new Intent(this, PlayGame.class);
+        startActivity(intent);
+
+
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        mapView.onResume();
-    }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
-    }
 }
---
+++ app/src/main/java/s1640402/coinzgame/nishtha_coinz/PlayGame.java
@@ -0,0 +1,221 @@
+package s1640402.coinzgame.nishtha_coinz;
+
+import android.location.Location;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineListener;
+import com.mapbox.android.core.location.LocationEnginePriority;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
+
+import java.util.List;
+
+
+public class PlayGame extends AppCompatActivity implements OnMapReadyCallback, LocationEngineListener,
+        PermissionsListener {
+
+    private MapView mapView;
+    private String tag = "PlayGame";
+    private MapboxMap map;
+
+    private PermissionsManager permissionsManager;
+    private LocationEngine locationEngine;
+    private LocationLayerPlugin locationLayerPlugin;
+    private Location originLocation;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, "pk.eyJ1IjoibmlzaHRoYWt1bWFyIiwiYSI6ImNqbW5rbXdlaDBzYmYza254eGE1aXJkN2wifQ.Y2hUSRk2rGB45RKqgycCXQ");
+        setContentView(R.layout.activity_play_game);
+        mapView = (MapView) findViewById(R.id.mapboxMapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+
+    private void enableLocation()
+    {
+        if (PermissionsManager.areLocationPermissionsGranted(this))
+        {
+            Log.d(tag,"Permissions are granted");
+            initializeLocationEngine();
+            initializeLocationLayer();
+        }
+        else
+        {
+            Log.d(tag, "Permissions are not granted");
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(MapboxMap mapboxMap)
+    {
+        if (mapboxMap == null)
+        {
+            Log.d(tag, "[onMapReady] mapBox is null");
+        }
+        else
+        {
+            map = mapboxMap;
+            // Set user interface options
+            map.getUiSettings().setCompassEnabled(true);
+            map.getUiSettings().setZoomControlsEnabled(true);
+            // Make location information available
+            enableLocation();
+        }
+    }
+
+
+    private void setCameraPosition(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(),
+                location.getLongitude());
+        map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void initializeLocationEngine() {
+        locationEngine = new LocationEngineProvider(this)
+                .obtainBestLocationEngineAvailable();
+        locationEngine.setInterval(5000); // preferably every 5 seconds
+        locationEngine.setFastestInterval(1000); // at most every second
+        locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+        locationEngine.activate();
+
+        Location lastLocation = locationEngine.getLastLocation();
+        if (lastLocation != null) {
+            originLocation = lastLocation;
+            setCameraPosition(lastLocation);
+        } else {
+            locationEngine.addLocationEngineListener(this);
+        }
+    }
+
+
+    @SuppressWarnings("MissingPermission")
+    private void initializeLocationLayer()
+    {
+        if (mapView == null)
+        {
+            Log.d(tag, "mapView is null");
+        }
+        else
+        {
+            if (map == null)
+            {
+                Log.d(tag, "map is null");
+            }
+            else
+            {
+                locationLayerPlugin = new LocationLayerPlugin(mapView,
+                        map, locationEngine);
+                locationLayerPlugin.setLocationLayerEnabled(true);
+                locationLayerPlugin.setCameraMode(CameraMode.TRACKING);
+                locationLayerPlugin.setRenderMode(RenderMode.NORMAL);
+            }
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+        if (location == null)
+        {
+            Log.d(tag, "[onLocationChanged] location is null");
+        }
+        else
+        {
+            Log.d(tag, "[onLocationChanged] location is not null");
+            originLocation = location;
+            setCameraPosition(location);
+        }
+    }
+
+    @Override
+    @SuppressWarnings("MissingPermission")
+    public void onConnected() {
+        Log.d(tag, "[onConnected] requesting location updates");
+        locationEngine.requestLocationUpdates();
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain)
+    {
+        Log.d(tag, "Permissions: " + permissionsToExplain.toString());
+        // Present toast or dialog.
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted)
+    {
+        Log.d(tag, "[onPermissionResult] granted == " + granted);
+        if (granted)
+        {
+            enableLocation();
+        }
+        else
+        {
+           // Open a dialogue with the user
+        }
+    }
+
+
+        @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+}
